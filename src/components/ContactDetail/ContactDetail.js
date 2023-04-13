741097a9e59b62bdc2514a46937fae701fe80143
Hamun Javadzadeghan <116430962+Hamuon@users.noreply.github.com>
2023-04-13 13:45:04 +0330

    changes applied

--- src/components/ContactDetail/ContactDetail.js
+++ src/components/ContactDetail/ContactDetail.js
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
-const ContactDetail = ({ location }) => {
-  const { contact } = location.state;
-  
+import { Link, useLocation } from "react-router-dom";
+const ContactDetail = () => {
+  const location = useLocation();
+  const { state } = location;
+
   //   console.log(props.location.state.contact);
   return (
     <div>
-      <p> user name is : {contact.name}</p>
-      <p> user email is : {contact.email}</p>
+      <p> user name is : {state.contact.name}</p>
+      <p> user email is : {state.contact.email}</p>
       <Link to="/"> go to contact List</Link>
     </div>
   );
--- src/components/ContactList/Contact/Contact.js
+++ src/components/ContactList/Contact/Contact.js
@@ -1,14 +1,13 @@
 import userImage from "../../../assets/images/user.png";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Contact = ({ contact, onDelete }) => {
   const { name, email, id } = contact;
-
   return (
     <div key={id} className="item">
       <div style={{ display: "flex", alignItems: "center" }}>
         <img src={userImage} alt="user" />
-        <Link to={{ pathname: `user/${id}`, state: { contact: contact } }}>
+        <Link to={`user/${id}`} state={{ contact }}>
           <div className="user">
             <p> name : {name}</p>
             <p> email : {email}</p>
@@ -17,7 +16,7 @@ const Contact = ({ contact, onDelete }) => {
       </div>
       <div>
         <Link to={`/edit/${id}`}>
-          <button className='editBtn'>Edit</button>
+          <button className="editBtn">Edit</button>
         </Link>
         <button onClick={() => onDelete(id)}>delete</button>
       </div>
